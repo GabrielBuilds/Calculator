6c6fde5b140e54a76acb2943bf68608080c47c08
GabrielBuilds <gabrieljsilvaernesto@gmail.com>
2026-03-13 15:47:24 -0300

    Calculadora

---
+++ callcc.py
@@ -0,0 +1,16 @@
+numero1 = float(input('Digite o primeiro numero: '))
+numero2 = float(input('Digite o segundo numero: '))
+
+print('Escolha a operação:\n1-Adição\n2-Subtração\n3-Multiplicação\n4-Divisão')
+
+operacao = input('Digite o numero da operação desejada: ')
+
+if operacao == '1':
+    resultado = numero1 + numero2
+elif operacao == '2':
+    resultado = numero1 - numero2
+elif operacao == '3':
+    resultado = numero1 * numero2
+elif operacao == '4':
+    resultado = numero1 / numero2
+print('O resultado da operação é de {:.3}' .format(resultado))
